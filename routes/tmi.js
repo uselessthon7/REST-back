4fbd2f73ea19557a4a5ce05a3d5fac4d2e7d3ee9
1ilsang <1ilsang@naver.com>
2019-05-19 06:52:10 +0900

    GET] /tmi/q=?

--- routes/tmi.js
+++ routes/tmi.js
@@ -3,40 +3,44 @@ const multer = require('multer');
 const multerGoogleStorage = require('multer-google-storage');
 const upload = multer({
     storage: multerGoogleStorage.storageEngine({
-        bucket:'uselessthon7',
-        projectId:'fluent-century-187304',
-        keyFilename:'config/gcp_storage_keyfile.json'
+        bucket: 'uselessthon7',
+        projectId: 'fluent-century-187304',
+        keyFilename: 'config/gcp_storage_keyfile.json'
     }),
-    limits: { fileSize: 5 * 1024 * 1024 }
+    limits: {fileSize: 5 * 1024 * 1024}
 });
 const router = express.Router();
-const mysql      = require('mysql');
-const dbconfig   = require('../config/database.js');
+const mysql = require('mysql');
+const dbconfig = require('../config/database.js');
 const connection = mysql.createConnection(dbconfig);
 connection.connect();
 
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
     res.render('fileupload');
 });
 
 router.post('/', upload.single('userfile'), function (req, res, next) {
-    //console.log(req.body.category[0]);
-    // res.send(req.file);
-
     const SQL = `INSERT INTO description(title, description, url) VALUES('\''${req.body.title}'\'', '\''${req.body.desc}'\'', '\''${req.file.path}'\'')`;
-    connection.query(SQL, function(err, rows) {
-        if(err) throw err;
+    connection.query(SQL, function (err, rows) {
+        if (err) throw err;
 
-        console.log(rows.insertId);
+        let list = req.body.category;
+        for (let i = 0; i < list.length; i++) {
+            connection.query(`INSERT INTO category_has_description VALUES(?, ?)`, [list[i], rows.insertId], function (err, rows) {
+                if (err) throw err;
+            });
+        }
+        res.status(200);
+    });
+});
 
-	let list = req.body.category;
-	for(let i = 0; i < list.length; i++) {
-	  
-	  connection.query(`INSERT INTO category_has_description VALUES(?, ?)`, [list[i], rows.insertId], function(err, rows) {
-		  if(err) throw err;
-	  });
-	}
-	    res.send('100');
+router.get('/:q', function(req, res, next) {
+    let list = req.params.q.slice(2);
+    const SQL = 'SELECT * FROM description WHERE seq IN (SELECT d_seq FROM category_has_description WHERE c_seq IN (?))';
+    connection.query(SQL, [list], function (err, rows) {
+        if(err)  throw err;
+
+        res.json(rows);
     });
 });
 
