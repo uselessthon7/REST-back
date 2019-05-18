d669c7afb9e6c5837361bd55cc9badde6c097988
1ilsang <1ilsang@naver.com>
2019-05-19 07:34:34 +0900

    test

--- routes/tmi.js
+++ routes/tmi.js
@@ -21,25 +21,41 @@ router.get('/test', function (req, res, next) {
 });
 
 router.post('/', upload.single('userfile'), function (req, res, next) {
+
+    let pr = function () {
+        return new Promise(function (resolve, reject) {
+            resolve('success');
+        });
+    };
+
+
     const SQL = `INSERT INTO description(title, description, url) VALUES('\''${req.body.title}'\'', '\''${req.body.desc}'\'', '\''${req.file.path}'\'')`;
     connection.query(SQL, function (err, rows) {
         if (err) throw err;
 
         let list = req.body.category;
-        for (let i = 0; i < list.length; i++) {
-            connection.query(`INSERT INTO category_has_description VALUES(?, ?)`, [list[i], rows.insertId], function (err, rows) {
-                if (err) throw err;
-            });
-        }
-        res.status(200);
+        pr().then(function (r) {
+            for (let i = 0; i < list.length; i++) {
+                connection.query(`INSERT INTO category_has_description VALUES(?, ?)`, [list[i], rows.insertId], function (err, rows) {
+                    if (err) throw err;
+                });
+            }
+        }).then(() => res.status(200));
+        // for (let i = 0; i < list.length; i++) {
+        //     connection.query(`INSERT INTO category_has_description VALUES(?, ?)`, [list[i], rows.insertId], function (err, rows) {
+        //         if (err) throw err;
+        //     });
+        // }
+        // res.status(200);
     });
 });
 
-router.get('/', function(req, res, next) {
+
+router.get('/', function (req, res, next) {
     let list = req.query.q;
     const SQL = 'SELECT * FROM description WHERE seq IN (SELECT d_seq FROM category_has_description WHERE c_seq IN (?)) ORDER BY rand() LIMIT 10';
     connection.query(SQL, [list], function (err, rows) {
-        if(err)  throw err;
+        if (err) throw err;
         res.json(rows);
     });
 });
