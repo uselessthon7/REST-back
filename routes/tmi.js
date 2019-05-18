e23a25db9737b985b2e986a27085ead4f564c518
1ilsang <1ilsang@naver.com>
2019-05-19 00:10:26 +0900

    Img upload

--- routes/tmi.js
+++ routes/tmi.js
@@ -1,9 +1,29 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const multer = require('multer');
+const multerGoogleStorage = require('multer-google-storage');
+const upload = multer({
+    storage: multerGoogleStorage.storageEngine({
+        bucket:'uselessthon7',
+        projectId:'fluent-century-187304',
+        keyFilename:'config/gcp_storage_keyfile.json',
+    }),
+    limits: { fileSize: 5 * 1024 * 1024 }
+});
+const router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-    res.send(req.query.q);
+    res.render('fileupload');
+});
+
+router.post('/', upload.single('userfile'), function (req, res, next) {
+    console.log(req.file);
+    console.log(req.body.title);
+    //console.log(req.body.category[0]);
+
+    // TODO img 리사이징
+
+    res.json({ url: req.file.path });
 });
 
 module.exports = router;
